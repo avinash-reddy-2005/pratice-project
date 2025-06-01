acdd9d9254847f4584dd1fc484b802947a577ff3
Chappidi Avinash Reddy <cavinashreddy2005@gmail.com>
2025-06-01 13:34:21 +0530

    Add files via upload

---
+++ Main10.java
@@ -0,0 +1,16 @@
+class Main10
+{
+    public static void main(String args[])
+    {
+        for (int i=0;i<5;i++)
+        {
+            for(int j=0;j<i+1;j++)
+            {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        
+
+    }
+}
